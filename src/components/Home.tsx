1596bd97dfde11feca19dc1c1ea91ec200013ab7
Supachart Tansutiraphong <>
2020-05-04 22:38:25 +0700

    add anime on Home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,21 +1,55 @@
 import * as React from "react";
+import anime from 'animejs';
 
 import '../assets/scss/Home.scss';
 
-const Home = () => (
+const Home = ({
+  display,
+}) => (
   <div className="Home">
     <div className="Home__heroBanner">
-      <h1>
-        <span>Hi,</span>
-        <br />
-        <span>I'm </span>
-        <span className="Home__hero">Supachart Tansutiraphong.</span>
-        <br />
-        <span>Full-stack developer.</span>
+      <h1 className="Home__ml" dangerouslySetInnerHTML={display}>
       </h1>
     </div>
     <div className="Home__submit">CONTACT ME</div>
   </div>
 );
 
-export default Home;
+export default () => {
+  const heroTexts = [
+    'Hi,',
+    "I'm ",
+    'Supachart Tansutiraphong.',
+    'Full-stack developer.',
+  ];
+  const innerText = heroTexts.reduce((element, word, i) => {
+    if (i === 2) {
+      element.push(word.replace(/\S/g, "<span class='letter letter--red'>$&</span>"));
+    }
+    else {
+      element.push(word.replace(/\S/g, "<span class='letter'>$&</span>"));
+    }
+    if (i != 1) {
+      element.push('<br />');
+    }
+    return element;
+  }, [])
+  const display = {
+    __html: innerText.join(''),
+  };
+
+  React.useEffect(() => {
+    anime.timeline().add({
+      targets: '.Home__ml .letter',
+      opacity: [0, 1],
+      translateY: [-100, 0],
+      easing: "easeOutExpo",
+      duration: 1400,
+      delay: (el, i) => 30 * i
+    });
+  });
+
+  return Home({
+    display,
+  });
+};
