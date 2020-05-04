16c0e18d364ef9b375e56205450706469ade3847
Supachart Tansutiraphong <>
2020-05-04 13:19:29 +0700

    add active navigation link

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,20 +1,26 @@
 import * as React from "react";
 import { element } from 'prop-types';
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import './Layout.scss';
 
-const Layout = ({ children }) => (
+const Layout = ({
+  navigation,
+  children,
+}) => (
   <div className="Layout">
     <header>
       <nav className="Layout__header">
-        <Link to='/' >Home</Link>
-        <div>About</div>
-        <Link to='/skills'>Skills</Link>
-        <Link to='/experience'>Experience</Link>
-        <Link to='/contact'>Contact</Link>
+        {navigation.map(({ path, label }) => (
+          <NavLink
+            key={label}
+            exact={path === '/'}
+            activeClassName="Layout__header--active"
+            to={path}
+          >{label}</NavLink>
+        ))}
       </nav>
     </header>
     <main className="Layout__content">{children}</main>
@@ -35,4 +41,17 @@ Layout.propTypes = {
   children: element.isRequired,
 };
 
-export default Layout;
+export default ({ children }) => {
+  const navigation = [
+    { path: '/', label: 'Home' },
+    { path: '/about', label: 'About' },
+    { path: '/skills', label: 'Skills' },
+    { path: '/experience', label: 'Experience' },
+    { path: '/contact', label: 'Contact' },
+  ];
+
+  return Layout({
+    navigation,
+    children,
+  });
+}
