7f8e159b4c7bdbec6d8c0f0e01c230d6edf2b66a
Supachart Tansutiraphong <supachart.ta@easysunday.com>
2020-05-09 11:36:54 +0700

    feat(about): add page

---
+++ src/components/About/About.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+import './About.scss';
+
+const About = () => (
+  <div className="About">
+    <div className="About__title">
+      <h1>About me</h1>
+    </div>
+    <div className="About__fall">
+      <p>An enthusiastic and self-motivated Software Engineer with an experience in designing, developing large scale software. Over 8 year of experience as a polyglot full-stack developer with strong skills in architecture design, introducing best practices for development and CI/CD....</p>
+    </div>
+  </div>
+);
+
+export default About;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,11 +5,13 @@ import Experience from "../components/Experience";
 import Skills from "../components/Skills";
 import Layout from "../components/Layout";
 import Contact from "../components/Contact";
+import About from "../components/About";
 
 const Routes = () => (
   <Layout>
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
       <Route path="/experience" component={Experience} />
       <Route path="/contact" component={Contact} />
       <Route path="/skills" component={Skills} />
